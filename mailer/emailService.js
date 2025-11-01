91518ae3ba7699b7bbca079da34554dde33710a7
JeeN <147690684+SerenityJN@users.noreply.github.com>
2025-11-01 13:36:45 +0800

    Use environment variables for SMTP configuration

--- mailer/emailService.js
+++ mailer/emailService.js
@@ -2,8 +2,8 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
   secure: true,
   auth: {
     user: process.env.SMTP_USER,
